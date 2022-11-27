6e1af80527430c6520adc13b8e3982bfd6732f2d
Samuel Adegalu <samuelhanana@gmail.com>
2022-11-27 12:34:26 +0000

    C program

---
+++ git/c/char.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+	int x = 'A';
+	int y = 'B';
+	int d = 'C';
+	printf("The numerical value for A is %d\n", x);
+	printf("The numerical value for B is %d\n", y);
+	printf("The numerical value for C is %d\n", d);
+	return 0;
+}	
---
+++ git/c/print.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	printf("I'm printing using C programming\n");
+	return 0;
+}
